1421ae651062df2470e055393123b8526f05908b
Tillkoeln <Tillkoeln@coin.com>
2016-05-29 21:15:08 +0200

    Hello World

---
+++ README.md
@@ -0,0 +1,6 @@
+
+S.C.A.M development tree
+
+S.C.A.M is a PoS-based cryptocurrency.
+
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define S.C.A.M-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "S.C.A.MURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
